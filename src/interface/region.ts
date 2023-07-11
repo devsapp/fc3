3fba4add00ed1cb2c8ac71d2ea91b784013be6c8
wss-git <wssgryx@163.com>
2023-07-11 16:24:03 +0800

    feat: 新增 参数说明

---
+++ src/interface/region.ts
@@ -0,0 +1,25 @@
+
+export enum Region {
+  'cn-hangzhou' = 'cn-hangzhou',
+  'cn-shanghai' = 'cn-shanghai',
+  'cn-qingdao' = 'cn-qingdao',
+  'cn-beijing' = 'cn-beijing',
+  'cn-zhangjiakou' = 'cn-zhangjiakou',
+  'cn-huhehaote' = 'cn-huhehaote',
+  'cn-shenzhen' = 'cn-shenzhen',
+  'cn-chengdu' = 'cn-chengdu',
+  'cn-hongkong' = 'cn-hongkong',
+  'ap-southeast-1' = 'ap-southeast-1',
+  'ap-southeast-2' = 'ap-southeast-2',
+  'ap-southeast-3' = 'ap-southeast-3',
+  'ap-southeast-5' = 'ap-southeast-5',
+  'ap-southeast-7' = 'ap-southeast-7',
+  'ap-northeast-1' = 'ap-northeast-1',
+  'eu-central-1' = 'eu-central-1',
+  'eu-west-1' = 'eu-west-1',
+  'us-east-1' = 'us-east-1',
+  'us-west-1' = 'us-west-1',
+  'ap-south-1' = 'ap-south-1',
+}
+
+export type IRegion = `${Region}`;
