cca7cabb87be23618af110347a20fa6149867cce
wss-git <wssgryx@163.com>
2023-07-24 09:52:31 +0800

    style: 🎨 format

--- src/base.ts
+++ src/base.ts
@@ -21,8 +21,14 @@ export default class Base {
           option: [
             ['-y, --yes', 'Configuring deployment using yaml'],
             ['--skip-push', 'Skip Mirror Push'],
-            ["--function ['code'/'config']", "Deploy function only; Use 'code' to deploy function code only, use 'config' to deploy function configuration only"],
-            ['--trigger [triggerName]', "Deploy trigger only; Specify a trigger name to deploy only the specified triggerName; Multiple names can be used ',' split"],
+            [
+              "--function ['code'/'config']",
+              "Deploy function only; Use 'code' to deploy function code only, use 'config' to deploy function configuration only",
+            ],
+            [
+              '--trigger [triggerName]',
+              "Deploy trigger only; Specify a trigger name to deploy only the specified triggerName; Multiple names can be used ',' split",
+            ],
           ],
         },
       },
