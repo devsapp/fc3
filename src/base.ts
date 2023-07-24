f301dc11698fa8669fa4f16c5909eb42f221a56e
wss-git <wssgryx@163.com>
2023-07-24 09:51:57 +0800

    feat: add deploy help

--- src/base.ts
+++ src/base.ts
@@ -19,13 +19,11 @@ export default class Base {
           description: 'deploy command',
           summary: '',
           option: [
-            ['--no-enable', 'whether to no enable proxy'],
-            ['--abc', 'xxxx'],
+            ['-y, --yes', 'Configuring deployment using yaml'],
+            ['--skip-push', 'Skip Mirror Push'],
+            ["--function ['code'/'config']", "Deploy function only; Use 'code' to deploy function code only, use 'config' to deploy function configuration only"],
+            ['--trigger [triggerName]', "Deploy trigger only; Specify a trigger name to deploy only the specified triggerName; Multiple names can be used ',' split"],
           ],
-        },
-        subCommands: {
-          service: {},
-          function: {},
         },
       },
       remove: {},
