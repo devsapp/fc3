c3a392e660f9eb918662e241ddb9145245d7aa07
wss-git <wssgryx@163.com>
2023-07-26 09:50:34 +0800

    feat: 支持 instanceConcurrency 默认值

--- src/default/config.ts
+++ src/default/config.ts
@@ -1,8 +1,13 @@
 export const FUNCTION_DEFAULT_CONFIG = {
   internetAccess: true,
   description: '',
-  // instanceConcurrency: 1, // TODO: custom
 };
+
+export const FUNCTION_CUSTOM_DEFAULT_CONFIG = {
+  internetAccess: true,
+  description: '',
+  instanceConcurrency: 1,
+}
 
 export const FC_RESOURCES_EMPTY_CONFIG = {
   vpcConfig: {
