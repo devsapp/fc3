ba7bd40ed4de35f4844f6c818ca15367a532de4e
wss-git <wssgryx@163.com>
2023-07-27 15:28:22 +0800

    fix: deploy code 还会部署配置
    Signed-off-by: wss-git <wssgryx@163.com>

--- __tests__/examples/nodejs/code/http.js
+++ __tests__/examples/nodejs/code/http.js
@@ -1,8 +1,3 @@
-var getRawBody = require('raw-body');
-var getFormBody = require('body/form');
-var body = require('body');
-
-
 /*
 To enable the initializer feature (https://help.aliyun.com/document_detail/156876.html)
 please implement the initializer function as below：
@@ -13,38 +8,6 @@ exports.initializer = (context, callback) => {
 */
 
 exports.handler = (req, resp, context) => {
-    console.log('hello world');
-// 业务代码
-    resp.send(JSON.stringify(params, null, '    '));
-
-    // var params = {
-    //     path: req.path,
-    //     queries: req.queries,
-    //     headers: req.headers,
-    //     method : req.method,
-    //     requestURI : req.url,
-    //     clientIP : req.clientIP,
-    // }
-
-    // getRawBody(req, function(err, body) {
-    //     for (var key in req.queries) {
-    //       var value = req.queries[key];
-    //       resp.setHeader(key, value);
-    //     }
-    //     resp.setHeader('Content-Type', 'application/json');
-    //     params.body = body.toString();
-    //     resp.send(JSON.stringify(params, null, '    '));
-    // });
-
-    /*
-    getFormBody(req, function(err, formBody) {
-        for (var key in req.queries) {
-          var value = req.queries[key];
-          resp.setHeader(key, value);
-        }
-        params.body = formBody;
-        console.log(formBody);
-        resp.send(JSON.stringify(params));
-    });
-    */
+  console.log('hello world', req);
+  resp.send(JSON.stringify({}, null, '    '));
 }
