3d4f90c0f2c0ea053fb9855cd673f267273a8d8f
wss-git <wssgryx@163.com>
2023-08-11 11:24:47 +0800

    feat: remove version 需要有提示

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import inquirer from 'inquirer';
 
 export { default as verify } from './verify';
 export { default as runCommand } from './run-command';
@@ -18,3 +19,16 @@ export const getTimeZone = (): string => {
   const timeZone = 'UTC+' + (0 - new Date().getTimezoneOffset() / 60);
   return timeZone;
 };
+
+export async function promptForConfirmOrDetails(message: string): Promise<boolean> {
+  const answers: any = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'prompt',
+      message,
+      choices: ['yes', 'no'],
+    },
+  ]);
+
+  return answers.prompt === 'yes';
+}
