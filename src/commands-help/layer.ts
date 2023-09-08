98a9c060518f64b4d38895a163ce16569c5f3de2
starink <shiyifan198@163.com>
2023-09-08 11:14:18 +0800

    add alias help

--- src/commands-help/layer.ts
+++ src/commands-help/layer.ts
@@ -9,9 +9,15 @@ export default {
         description: 'Publish new layer version',
         summary: 'Publish new layer version',
         option: [
-          ['--region <region>', '[C-Required] Specify the fc regions of the layer, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability'],
+          [
+            '--region <region>',
+            '[C-Required] Specify the fc regions of the layer, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
           ['--code <code>', '[Required] Specify the code path of the layer'],
-          ['--compatible-runtime <compatibleRuntime>', '[Optional] Specify the compatible runtimes of the layer, you can see all compatible runtimes in https://www.alibabacloud.com/help/en/fc/user-guide/overview-of-runtimes'],
+          [
+            '--compatible-runtime <compatibleRuntime>',
+            '[Optional] Specify the compatible runtimes of the layer, you can see all compatible runtimes in https://www.alibabacloud.com/help/en/fc/user-guide/overview-of-runtimes',
+          ],
           ['--description <description>', '[Optional] Specify the description of the layer'],
           ['--layer-name <layerName>', '[Required] Specify the name of the layer'],
         ],
@@ -22,10 +28,16 @@ export default {
         description: 'Get layer list',
         summary: 'Get layer list',
         option: [
-          ['--region <region>', '[C-Required] Specify the fc regions, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability'],
+          [
+            '--region <region>',
+            '[C-Required] Specify the fc regions, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
           ['--prefix <prefix>', '[Optional] Specify the prefix of layer name'],
           ['--public <public>', '[Optional] Specify if show personal public layers'],
-          ['--official <official>', '[Optional] Specify if show official public layers. If you set "official" as true, "public" will be automatically set as true, which means setting "public" as false is void'],
+          [
+            '--official <official>',
+            '[Optional] Specify if show official public layers. If you set "official" as true, "public" will be automatically set as true, which means setting "public" as false is void',
+          ],
           ['--table <table>', '[Optional] Specify if output the result as table format'],
         ],
       },
@@ -35,7 +47,10 @@ export default {
         description: 'Get layer version detail',
         summary: 'Get layer version detail',
         option: [
-          ['--region <region>', '[C-Required] Specify the fc regions of the layer, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability'],
+          [
+            '--region <region>',
+            '[C-Required] Specify the fc regions of the layer, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
           ['--layer-name <layernName>', '[Required] Specify the name of the layer'],
           ['--version-id <versionId>', '[Required] Specify the version id of the layer version'],
         ],
@@ -46,7 +61,10 @@ export default {
         description: 'Get layer versions ',
         summary: 'Get layer versions ',
         option: [
-          ['--region <region>', '[C-Required] Specify the fc regions of the layer, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability'],
+          [
+            '--region <region>',
+            '[C-Required] Specify the fc regions of the layer, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
           ['--layer-name <layerName>', '[Required] Specify the name of the layer version'],
           ['--table <table>', '[Optional] Specify if output the result as table format'],
         ],
@@ -57,9 +75,18 @@ export default {
         description: 'Download layer version code',
         summary: 'Download layer version code',
         option: [
-          ['--region <region>', '[C-Required] Specify the fc regions of the layer, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability'],
-          ['--layer-name <layerName>', '[Optional] Specify the name of the layer version. If you do not set "arn", "layer-name" is required.'],
-          ['--version-id <versionId>', '[Optional] Specify the version id of the layer version. If you do not set "arn", "version-id" is required.'],
+          [
+            '--region <region>',
+            '[C-Required] Specify the fc regions of the layer, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
+          [
+            '--layer-name <layerName>',
+            '[Optional] Specify the name of the layer version. If you do not set "arn", "layer-name" is required.',
+          ],
+          [
+            '--version-id <versionId>',
+            '[Optional] Specify the version id of the layer version. If you do not set "arn", "version-id" is required.',
+          ],
           ['--arn <arn>', '[Optional] Specify the ARN (Aliyun Resource Name) of the layer version'],
         ],
       },
@@ -69,9 +96,15 @@ export default {
         description: 'Set the layer as publish or private',
         summary: 'Set the layer as publish or private',
         option: [
-          ['--region <region>', '[C-Required] Specify the fc regions of the layer, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability'],
+          [
+            '--region <region>',
+            '[C-Required] Specify the fc regions of the layer, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
           ['--layer-name <layerName>', '[Required] Specify the name of the layer version'],
-          ['--public <public>', '[Optional] Specify if the layer is public. If you do not use this key, the layer will be set private'],
+          [
+            '--public <public>',
+            '[Optional] Specify if the layer is public. If you do not use this key, the layer will be set private',
+          ],
         ],
       },
     },
@@ -80,7 +113,10 @@ export default {
         description: 'Remove layer version',
         summary: 'Remove layer version',
         option: [
-          ['--region <region>', '[C-Required] Specify the fc regions of the layer, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability'],
+          [
+            '--region <region>',
+            '[C-Required] Specify the fc regions of the layer, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
           ['--layer-name <layerName>', '[Required] Specify the name of the layer'],
           ['--version-id <versionId>', '[Required] Specify the version id of the layer version'],
         ],
