960eb7adcb6413e6212d217136d7a7fb04e40b36
starink <shiyifan198@163.com>
2023-09-08 11:14:18 +0800

    add alias help

--- src/commands-help/alias.ts
+++ src/commands-help/alias.ts
@@ -1,6 +1,7 @@
 export default {
   help: {
-    description: 'aliases command',
+    description: 'Function alias operation ',
+    summary: 'Function alias operation ',
   },
   subCommands: {
     get: {
@@ -8,7 +9,10 @@ export default {
         description: 'Get alias details',
         summary: 'Get alias details ',
         option: [
-          ['--region <region>', '[C-Required] Specify fc regions, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability'],
+          [
+            '--region <region>',
+            '[C-Required] Specify fc regions, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
           ['--function-name <functionName>', '[C-Required] Specify function name'],
           ['--alias-name <aliasName>', '[Required] Specify alias name'],
         ],
@@ -19,7 +23,10 @@ export default {
         description: 'View the list of function aliases',
         summary: 'View the list of function aliases',
         option: [
-          ['--region <region>', '[C-Required] Specify the fc regions, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability'],
+          [
+            '--region <region>',
+            '[C-Required] Specify the fc regions, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
           ['--function-name <functionName>', '[C-Required] Specify function name'],
           ['--table <table>', '[Optional] Specify if output the result as table format'],
         ],
@@ -30,13 +37,17 @@ export default {
         description: 'Publish function alias',
         summary: 'Publish function alias',
         option: [
-          ['--region <region>', '[C-Required] Specify the fc regions, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability'],
+          [
+            '--region <region>',
+            '[C-Required] Specify the fc regions, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
           ['--function-name <functionName>', '[C-Required] Specify function name'],
           ['--alias-name <aliasName>', '[Required] Specify alias name'],
           ['--description <description>', '[Optional] Specify alias description'],
+          ['--version-id <versionId>', '[Optional] Specify version id of the function'],
           [
             '--vw,--additionalVersionWeight <json>',
-            `Grayscale version and weights, e.g.: "{\\"1\\":0.2}"`,
+            `[Optional] Specify grayscale version and weights, e.g.: "{\\"1\\":0.2}"`,
           ],
         ],
       },
@@ -46,9 +57,12 @@ export default {
         description: 'Remove function alias',
         summary: 'Remove function alias',
         option: [
-          ['--region <region>', 'Appoint region'],
-          ['--function-name <functionName>', 'Appoint function name'],
-          ['--alias-name <aliasName>', 'Appoint alias name'],
+          [
+            '--region <region>',
+            '[C-Required] Specify the fc regions, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
+          ['--function-name <functionName>', '[C-Required] Specify function name'],
+          ['--alias-name <aliasName>', '[Required] Specify alias name'],
           ['-y, --yes', "Don't ask, delete directly"],
         ],
       },
