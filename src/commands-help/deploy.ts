3812b7a19bf6b0f1768977ba5311b8d34805f266
starink <shiyifan198@163.com>
2023-09-12 17:46:45 +0800

    add help

--- src/commands-help/deploy.ts
+++ src/commands-help/deploy.ts
@@ -1,17 +1,25 @@
 export default {
   help: {
-    description: 'deploy command',
-    // summary: '',
+    description:
+      'Deploy local resources online. \n' +
+      'Example: \n' +
+      '  $ s3 deploy \n' +
+      '  $ s3 deploy --skip-push \n' +
+      '  $ s3 deploy --function code \n' +
+      '  $ s3 deploy --skip-push --function config \n' +
+      '  $ s3 deploy --trigger triggerName \n' +
+      '  $ s3 deploy --skip-push --trigger triggerName1,trigggerName2',
+    summary: 'Deploy local resources online',
     option: [
       ['-y, --assume-yes', 'Configuring deployment using yaml'],
-      ['--skip-push', 'Skip Mirror Push'],
+      ['--skip-push', '[Optional] Specify if skip automatically pushing docker container images'],
       [
         "--function ['code'/'config']",
-        "Deploy function only; Use 'code' to deploy function code only, use 'config' to deploy function configuration only",
+        "[Optional] Only deploy function configuration or code. Use 'code' to deploy function code only, use 'config' to deploy function configuration only",
       ],
       [
         '--trigger [triggerName]',
-        "Deploy trigger only; Specify a trigger name to deploy only the specified triggerName; Multiple names can be used ',' split",
+        "[Optional] Only deploy trigger only. Specify a trigger name to deploy only the specified trigger; Multiple names can be split by ','",
       ],
     ],
   },
--- src/commands-help/provision.ts
+++ src/commands-help/provision.ts
@@ -1,49 +1,97 @@
 export default {
   help: {
-    description: 'provision command',
+    description: 'Function provision Operation',
+    summary: 'Function provision Operation',
   },
   subCommands: {
     list: {
       help: {
-        description: 'provision list command',
+        description: 'View the list of provision. \n' + 'Example: \n' + '  $ s3 provision list',
+        summary: 'View the list of provision',
         option: [
-          ['--region <region>', 'Appoint region'],
-          ['--function-name <functionName>', 'Appoint function name'],
+          [
+            '--region <region>',
+            '[C-Required] Specify fc region, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
+          ['--function-name <functionName>', '[C-Required] Specify function name'],
         ],
       },
     },
     get: {
       help: {
-        description: 'provision list command',
+        description:
+          'Get provision configuration. \n' +
+          'Example: \n' +
+          '  $ s3 provision get LATEST \n' +
+          '  $ s3 provision get test',
+        summary: 'Get provision configuration',
         option: [
-          ['--region <region>', 'Appoint region'],
-          ['--function-name <functionName>', 'Appoint function name'],
-          ['--qualifier <string>', 'Appoint qualifier'],
+          [
+            '--region <region>',
+            '[C-Required] Specify fc region, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
+          ['--function-name <functionName>', '[C-Required] Specify function name'],
+          [
+            '--qualifier <qualifier>',
+            '[Required] Specify the qualifier parameter. Only supports LATEST and alias',
+          ],
         ],
       },
     },
     put: {
       help: {
-        description: 'provision put command',
+        description:
+          'Set provision configuration. \n' +
+          'Example: \n' +
+          '  $ s3 provision put --qualifier test --target 2 \n' +
+          `  $ s3 provision put --qualifier test --ac --target 2 --scheduled-actions '[{"name":"scheduled-actions","startTime":"2023-08-15T02:04:00.000Z","endTime":"2033-08-15T02:04:00.000Z","target":1,"scheduleExpression":"cron(0 0 4 * * *)"}]' --target-tracking-policies '[{"name":"target-tracking-policies","startTime":"2023-08-15T02:05:00.000Z","endTime":"2033-08-15T02:05:00.000Z","metricType":"ProvisionedConcurrencyUtilization","metricTarget":0.6,"minCapacity":1,"maxCapacity":3}]'`,
+        summary: 'Set provision configuration',
         option: [
-          ['--region <region>', 'Appoint region'],
-          ['--function-name <functionName>', 'Appoint function name'],
-          ['--target <max>', 'Appoint provision target'],
-          ['--ac, --always-allocate-cpu', 'Appoint provision target'],
-          ['--scheduled-actions <json>', 'Scheduled actions'],
-          ['--target-tracking-policies <json>', 'Target tracking policies'],
-          ['--qualifier <string>', 'Appoint qualifier'],
+          [
+            '--region <region>',
+            '[C-Required] Specify fc region, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
+          ['--function-name <functionName>', '[C-Required] Specify function name'],
+          ['--target <max>', '[Optional] Specify the provision target parameter'],
+          [
+            '--ac, --always-allocate-cpu',
+            '[Optional] Specify if always allocate CPU resources to the provisioned instances',
+          ],
+          [
+            '--scheduled-actions <json>',
+            '[Optional] Set the configuration details of scheduled auto scaling.',
+          ],
+          [
+            '--target-tracking-policies <json>',
+            '[Optional] Set	the configuration details of metric-based auto scaling.',
+          ],
+          [
+            '--qualifier <qualifier>',
+            '[Required] Specify the qualifier parameter. Only supports LATEST and alias',
+          ],
         ],
       },
     },
     remove: {
       help: {
-        description: 'provision remove command',
+        description:
+          'Delete provision. \n' +
+          'Example: \n' +
+          '  $ s3 provision remove LATEST \n' +
+          '  $ s3 provision remove test -y',
+        summary: 'Delete provision',
         option: [
-          ['--region <region>', 'Appoint region'],
-          ['--function-name <functionName>', 'Appoint function name'],
-          ['-y, --assume-yes', "Don't ask, delete directly"],
-          ['--qualifier <string>', 'Appoint qualifier'],
+          [
+            '--region <region>',
+            '[C-Required] Specify fc region, you can see all supported regions in https://www.alibabacloud.com/help/en/fc/product-overview/region-availability',
+          ],
+          ['--function-name <functionName>', '[C-Required] Specify function name'],
+          [
+            '--qualifier <qualifier>',
+            '[Required] Specify the qualifier parameter. Only supports LATEST and alias',
+          ],
+          ,
+          ['-y, --assume-yes', "[Optional] Don't ask, delete directly"],
         ],
       },
     },
