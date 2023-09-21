1e5dece73fe0ab3472811e481f06ae04c84e6151
rsonghuster <ls_huster@163.com>
2023-09-21 17:24:17 +0800

    support sync cli mode

--- src/commands-help/plan.ts
+++ src/commands-help/plan.ts
@@ -4,12 +4,6 @@ export default {
 Example:
   $ s3 plan`,
     summary: 'Perceive resource change',
-    option: [
-      [
-        '--region <region>',
-        '[C-Required] Specify fc region, you can see all supported regions in https://www.alibabacloud.com/help/zh/fc/product-overview/region-availability',
-      ],
-      ['--function-name <functionName>', '[C-Required] Specify function name'],
-    ],
+    option: [],
   },
 };
