8c985ab112e4bca509b0ccc319d9ed3d1999f972
liuzewen99 <zewenliubs@163.com>
2025-10-29 16:47:06 +0800

    fix: scaling document

--- src/commands-help/scaling.ts
+++ src/commands-help/scaling.ts
@@ -52,7 +52,7 @@ Examples with CLI:
         description: `Set scaling configuration.
 
 Examples with Yaml:
-  $ s scaling put --qualifier test --min-instances 2 --horizontal-scaling-policies '[{"name":"cpu-policy","metricType":"CPUUtilization","metricTarget":0.6,"minInstances":1,"maxInstances":10,"startTime":"2023-08-15T02:05:00.000Z","endTime":"2033-08-15T02:55:00.000Z","timeZone":"UTC"}]' --scheduled-policies '[{"name":"scheduled-policy","scheduleExpression":"cron(0 0 4 * * *)","target":5,"startTime":"2023-08-15T02:04:00.000Z","endTime":"2033-08-15T03:04:00.000Z","timeZone":"UTC"}]'
+  $ s scaling put --qualifier test --min-instances 2
 
 Examples with CLI:
   $ s cli fc3 scaling put --qualifier LATEST --min-instances 2 --region cn-hangzhou --function-name test -a default`,
@@ -68,14 +68,6 @@ Examples with CLI:
             '[Required] Specify the qualifier parameter. Only supports LATEST and alias',
           ],
           ['--min-instances <number>', '[Optional] Specify the minimum instances parameter'],
-          [
-            '--horizontal-scaling-policies <json>',
-            '[Optional] Set the configuration details of horizontal scaling policies.',
-          ],
-          [
-            '--scheduled-policies <json>',
-            '[Optional] Set the configuration details of scheduled policies.',
-          ],
           ['--resident-pool-id <id>', '[Optional] Specify the resident pool ID'],
         ],
       },
